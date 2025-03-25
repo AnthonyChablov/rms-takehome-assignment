eb1e8ab535da6db9dc25240f09033eb81ebf4fda
AnthonyChablov <achablov@myseneca.ca>
2025-03-25 19:53:33 -0400

    feat: git add .

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,17 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import queryPlugin from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended, 
+      ...tseslint.configs.recommended, 
+      'plugin:@tanstack/query/recommended', 
+      'plugin:@tanstack/eslint-plugin-query/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +22,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@tanstack/query': queryPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
