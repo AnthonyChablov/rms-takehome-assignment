6da466084d714b4c499433176c64a3011055a94a
Anthony Chablov <achablov@myseneca.ca>
2025-03-28 19:30:12 -0400

    feat: add table pane

--- src/components/Earthquakes/Earthquakes.tsx
+++ src/components/Earthquakes/Earthquakes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEarthquakes } from '@/services/earthquakes/earthquakesQuery';
 import { useHighlightedEarthquakeContext } from '@/context/EarthQuakeContext';
 import PlotPane from './Panes/PlotPane/PlotPane';
+import TablePane from './Panes/TablePane/TablePane';
 
 const Earthquakes = () => {
   const earthquakesQuery = useEarthquakes();
@@ -17,8 +18,9 @@ const Earthquakes = () => {
   }
 
   return (
-    <div>
+    <div className="w-full flex">
       <PlotPane data={earthquakesQuery.data} />
+      <TablePane />
     </div>
   );
 };
