979fc7c8868b0d8d7edf6f28e236bf22b25f2d97
Anthony Chablov <achablov@myseneca.ca>
2025-04-07 11:08:11 -0400

    fix: import necessary files from vitest

--- src/components/Earthquakes/components/TablePane/hooks/utils/__tests__/compareValues.test.ts
+++ src/components/Earthquakes/components/TablePane/hooks/utils/__tests__/compareValues.test.ts
@@ -1,5 +1,7 @@
-import { compareValues } from '../compareValues';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
 import { vi } from 'vitest';
+import { compareValues } from '../compareValues';
 
 interface TestObject {
   id: number;
