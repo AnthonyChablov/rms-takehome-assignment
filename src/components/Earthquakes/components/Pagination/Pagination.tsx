fc75792fc930f5bd8dd5340fa83e85ab21a5fc5b
Anthony Chablov <achablov@myseneca.ca>
2025-04-28 14:59:35 -0400

    feat: add consistent drop down select styling

--- src/components/Earthquakes/components/Pagination/Pagination.tsx
+++ src/components/Earthquakes/components/Pagination/Pagination.tsx
@@ -39,22 +39,35 @@ function Pagination({
       <div className="flex items-center space-x-4">
         <label
           htmlFor="itemsPerPage"
-          className="text-md font-medium text-gray-800"
+          className="text-sm font-bold text-gray-700"
         >
           Items per page:
         </label>
-        <select
-          id="itemsPerPage"
-          className="border border-gray-300 rounded-md shadow-sm py-2 px-3 text-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-          value={itemsPerPage}
-          onChange={onItemsPerPageChange}
-        >
-          {pageSizeOptions.map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              {pageSize}
-            </option>
-          ))}
-        </select>
+        <div className="relative hover:cursor-pointer">
+          <select
+            id="itemsPerPage"
+            className="block appearance-none w-full bg-gray-200 border border-gray-200 
+            text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+            value={itemsPerPage}
+            onChange={onItemsPerPageChange}
+          >
+            {pageSizeOptions.map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            {/* Arrow icon for dropdown */}
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
       </div>
       <div className="flex items-center space-x-4">
         <ActionButton
